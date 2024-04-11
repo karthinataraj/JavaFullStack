30a04e04d636a553db215c15e4a886113c2591e2
karthinataraj <117477177+karthinataraj@users.noreply.github.com>
2024-04-11 17:28:08 +0530

    Create Output.java

---
+++ Treeset/Output.java
@@ -0,0 +1,12 @@
+Output:
+1.Add number
+  2.Find average
+  3.ExitEnter your choice 
+  2
+  The set is empty
+  1.Add number
+  2.Find average
+  3.Exit
+  Enter your choice 
+  3
+  Thank you for using the application
